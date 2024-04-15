a22771b3562f005ec2552d36fd0acc4f234b3569
BluCorDev <mesasennaf@gmail.com>
2024-04-14 21:53:12 -0300

    si

--- Assets/Scripts/PhotonExtensions.cs
+++ Assets/Scripts/PhotonExtensions.cs
@@ -9,6 +9,7 @@ public static class PhotonExtensions {
 
     private static readonly Dictionary<string, string> SPECIAL_PLAYERS = new() {
         ["96b3ff9946cdf2e6678279a2d523e75ddeb12034b976c59af67913ba20bb8c1f"] = "BluCor",
+        ["425df519e2d249abb8c78a51c57e2f6c9fef898fe7231a440295e7d4470ca272"] = "BluCorDev",
         ["3d45e2b8eb586e10d4f49e3964b99cb403fb7a4c4792c57b8a6b085bb203807a"] = "Foxyyy",
         ["ffd345731c31c49b66a2c6398d6489fd1f19fe717ef78e0174fc4cbe7d4828cf"] = "FrostyCake",
         ["316424c21a3513d5e6294e7854615f3d0c13b4aaf09b2a443d2cfe0d0046d006"] = "zomblebobble",
