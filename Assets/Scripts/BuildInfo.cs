9305b7ecea068b3ef6e2fddf539235d8455d54de
BluCorDev <mesasennaf@gmail.com>
2025-10-06 21:00:24 -0300

    yes

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-public static class BuildInfo{public static string BUILD_TIME = "20/9/2025 5:34:33 a. m.";}
+public static class BuildInfo{public static string BUILD_TIME = "2/10/2025 7:00:04 a. m.";}
